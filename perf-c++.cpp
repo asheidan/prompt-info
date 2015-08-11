fddb264759e9595cfd7e2d3a26a33f51b5acbd3c
Emil Eriksson <asheidan@gmail.com>
2015-08-11 22:06:29 +0200

    Add playground done i c++
    Not sure this is the best languaga. But it sure works
    for just playing around in.

---
+++ perf-c++.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+
+// green: \x1B[32m
+/**
+ * 256color:
+ * FG: \x1B[38;5;%dm
+ * BG: \x1B[48;5;%dm
+ */
+
+class TermSize {
+public:
+	TermSize() : cols(80), rows(24)
+	{
+#ifdef TIOCGSIZE
+		struct ttysize ts;
+		ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
+		cols = ts.ts_cols;
+		rows = ts.ts_lines;
+#elif defined(TIOCGWINSZ)
+		struct winsize &ts;
+		ioctl(STDIN_FILENO, TIOCGWINSZ, &ts);
+		cols = ts.ws_col;
+		rows = ts.ws_row;
+		#endif
+	}
+
+	int cols;
+	int rows;
+};
+
+void decorate(const char * const str)
+{
+}
+
+void all_colors(TermSize &size)
+{
+	int fieldcount = size.cols / 4;
+	for (int i = 0; i < 0xFF; ++i) {
+		printf("\x1B[38;5;%dm%03d\x1B[0m ", i, i);
+		if ((fieldcount - 1) == (i % fieldcount)) {
+			printf("\n");
+		}
+	}
+
+}
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv) {
+	TermSize size;
+
+	const char *envvar;
+
+	envvar = getenv("SHLVL");
+	if (NULL != envvar) {
+		decorate(envvar);
+	}
+
+	return 0;
+}
